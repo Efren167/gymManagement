59982d58b4f8a910ebf1d09303cbfe0d9d1aa034
efren <RAZOR@LAPTOP-IA968CDT.(none)>
2023-10-21 12:36:59 +0200

    eliminacion frontend de este repo

--- gymManagement/src/main/java/com/angef/gymManagement/entity/Subscriber.java
+++ gymManagement/src/main/java/com/angef/gymManagement/entity/Subscriber.java
@@ -1,6 +1,8 @@
 package com.angef.gymManagement.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class Subscriber {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	
