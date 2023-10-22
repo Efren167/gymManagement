e4c1b36133460b05a44554fb6b052949639f1ded
Kpsodia <kpsodia@gmail.com>
2023-10-22 23:30:16 +0200

    Modificacion para quitar la creacion de la tabla Management

--- gymManagement/src/main/java/com/angef/gymManagement/entity/Management.java
+++ gymManagement/src/main/java/com/angef/gymManagement/entity/Management.java
@@ -1,36 +1,16 @@
 package com.angef.gymManagement.entity;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "MANAGEMENT")
 
 public class Management {
 
-	@Id
-	@Column(name = "id")
-	private Long id;
-
-	@Column(name = "invoiced", nullable = false)
 	private boolean invoiced;
-
-	@Column(name = "access", nullable = false)
 	private boolean access;
 
 	public Management(boolean invoiced, boolean access) {
 		this.invoiced = invoiced;
 		this.access = access;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public boolean isInvoiced() {
