8beceaa592a5cdf727e3bbe76eb2be28de63e288
Kpsodia <kpsodia@gmail.com>
2023-10-23 23:12:37 +0200

    Commit de la nueva tabla management y la posible conexion entre tablas

--- gymManagement/src/main/java/com/angef/gymManagement/entity/Management.java
+++ gymManagement/src/main/java/com/angef/gymManagement/entity/Management.java
@@ -1,16 +1,52 @@
 package com.angef.gymManagement.entity;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-
+@Table(name = "Management")
 public class Management {
-	
+
+	@Id
+	@Column(name = "id")
+	private long Id;
+
+	@Column(name = "invoiced")
 	private boolean invoiced;
+
+	@Column(name = "access")
 	private boolean access;
 
-	public Management(boolean invoiced, boolean access) {
+	public Management(long id, boolean invoiced, boolean access) {
+		this.Id = id;
 		this.invoiced = invoiced;
 		this.access = access;
+	}
+	
+	@ManyToOne(optional = false)
+    private Subscriber subscriber;
+
+	public Subscriber getSubscriber() {
+		return subscriber;
+	}
+
+	public void setSubscriber(Subscriber subscriber) {
+		this.subscriber = subscriber;
+	}
+
+	public boolean isAccess() {
+		return access;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long Id) {
+		this.Id = Id;
 	}
 
 	public boolean isInvoiced() {
--- gymManagement/src/main/java/com/angef/gymManagement/entity/Subscriber.java
+++ gymManagement/src/main/java/com/angef/gymManagement/entity/Subscriber.java
@@ -1,18 +1,21 @@
 package com.angef.gymManagement.entity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
-@Entity@NoArgsConstructor
+@Entity
+@NoArgsConstructor
 @Table(name = "SUBSCRIBER")
 public class Subscriber {
-	
+
 	@Id
 	@Column(name = "id")
+
 	private Long id;
-	
+
 	@Column(name = "name", nullable = false)
 	private String name;
 	@Column(name = "surname", nullable = false)
@@ -25,52 +28,61 @@ public class Subscriber {
 	private String phone;
 	@Column(name = "invoiced", nullable = false)
 	private String invoiced;
-	
-	
+
 	public String getInvoiced() {
 		return invoiced;
 	}
+
 	public void setInvoiced(String invoiced) {
 		this.invoiced = invoiced;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	
+
 	public String getDni() {
 		return dni;
 	}
+
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
 
 }
